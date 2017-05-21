52603f261cf60ad04bf2e7705dd4c5d7466b279e
David Koller <kolldavi@gmail.com>
2017-05-21 20:45:45 +0000

    formated comments

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -23,10 +23,19 @@
     </div>
     <div class="row">
           <div class="col col-md-8 col-md-offset-2 lrgOffset">
+              <h3 class="comments_title"><span class="glyphicon glyphicon-comment"></span>{{$post->comments()->count()}} comments</h3>
               @foreach($post->comments as $comment)
                   <div class="comment">
-                      <p>Name: {{$comment->name}}</p>
-                      <p>Comment:<br> {{$comment->comment}}</p>
+                      <div class="author_info">
+                           <img src="{{ "https://www.gravatar.com/avatar/" . md5(strtolower(trim($comment->email))) . "?s=50&d=wavatar" }}" class="author_img">
+                            <div class="author_name">
+                                <h4>{{$comment->name}}</h4>
+                                <p class="author-time">{{ date('M j, Y h:ia',strtotime($comment->created_at)) }}</p>
+                            </div>
+                      </div>
+                      <div class="comment_content">
+                          {{$comment->comment}}
+                      </div>
                       <hr>
                   </div>
               @endforeach
